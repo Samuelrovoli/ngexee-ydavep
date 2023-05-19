d23a725aff2ac1b245b8902b74638cb228948d6a
Samuelrovoli <128751847+Samuelrovoli@users.noreply.github.com>
2023-05-19 18:56:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngexee-ydavep
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngexee-iua9jc)
---
+++ src/app/products.ts
@@ -0,0 +1,43 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Celular',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'Tablet',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Notebook',
+    price: 299,
+    description: ''
+  },
+  
+{
+    id: 4,
+    name: 'Computador',
+    price: 299,
+    description: ''
+
+  }
+    
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
